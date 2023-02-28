a1231f7ea1052d9d4b1bc07a1bd5187e3a17ae65
victor-ss13 <victor.sampaio2003@gmail.com>
2023-02-28 11:59:56 -0300

    sketch.js
    Arquivo principal. Nele é onde inserimos as funções criadas para serem executadas dentro de draw( ).

---
+++ sketch.js
@@ -0,0 +1,18 @@
+//funções principais
+
+function setup() {
+  createCanvas(500, 400);
+  somTrilha.loop();
+}
+
+function draw() {
+  background(imagemEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPonto();
+  marcaPonto();
+}
